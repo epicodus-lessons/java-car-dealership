183ff8f532402f8cf5d07bff2b131db855da1687
Courtney Phillips <courtney@epicodus.com>
2016-08-23 13:17:50 -0700

    Instantiate 4 new Vehicle objects in command line interface.

--- App.java
+++ App.java
@@ -1,4 +1,33 @@
 public class App {
  public static void main(String[] args) {
+
+   Vehicle hatchback = new Vehicle();
+    hatchback.mYear = 1994;
+    hatchback.mBrand = "Subaru";
+    hatchback.mModel = "Legacy";
+    hatchback.mMiles = 170000;
+    hatchback.mPrice = 4000;
+
+    Vehicle suv = new Vehicle();
+    hatchback.mYear = 2002;
+    hatchback.mBrand = "Ford";
+    hatchback.mModel = "Explorer";
+    hatchback.mMiles = 100000;
+    hatchback.mPrice = 7000;
+
+    Vehicle sedan = new Vehicle();
+    hatchback.mYear = 2015;
+    hatchback.mBrand = "Toyota";
+    hatchback.mModel = "Camry";
+    hatchback.mMiles = 50000;
+    hatchback.mPrice = 30000;
+
+    Vehicle truck = new Vehicle();
+    hatchback.mYear = 1999;
+    hatchback.mBrand = "Ford";
+    hatchback.mModel = "Ranger";
+    hatchback.mMiles = 100000;
+    hatchback.mPrice = 4000;
+    
  }
 }
